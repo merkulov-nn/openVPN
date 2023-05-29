e1171b2847f4e7bd26ff4c2a3201f1a90345ee6e
merkulov-nn <merkulovnn.work@gmail.com>
2023-05-29 16:59:41 +0300

    lesson34

---
+++ ras2/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5","< 7.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"3d494f398b5aa7e189105cf3ec881a5ee39c74b7c142650d20a514a5b0c6525e","vagrant_version":"2.2.19"}
